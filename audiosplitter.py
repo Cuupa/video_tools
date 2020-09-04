fca7a1d0aa58442713ade7166485e598ecff893e
Cuupa <simon.thiel2+github@gmail.com>
2020-09-05 00:20:48 +0200

    Update audiosplitter.py

--- audiosplitter.py
+++ audiosplitter.py
@@ -1,5 +1,9 @@
 import moviepy.editor as mp
 
 def get_audio(file) -> AudioFileClip:
-  videoclip = mp.VideoFileClip("Video File") 
+  videoclip = mp.VideoFileClip(file) 
   return videoclip.audio
+
+def save_audio(file, dest):
+  videoclip = mp.VideoFileClip(file) 
+  videoclip.write_audiofile(dest) 
