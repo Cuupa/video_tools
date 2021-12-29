83bfad0868842b81203722015aed8f185f95542a
simon.thiel2+github@gmail.com <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>
2021-12-29 14:11:13 +0100

    handbrake script

--- HandBrake.py
+++ HandBrake.py
@@ -1,63 +1,175 @@
 import os.path
+import platform
+import shutil
 import subprocess
 from os import path, walk
 from os.path import expanduser
 
-'''
-ffprobe_language = "{filename} -show_entries stream=index:stream_tags=language -select_streams a -of compact=p=0:nk=1"
-'''
-
 journal = "journal.log"
 
-resultpath = path.join("~", "Convert", "Result")
-sourcepath = path.join("~", "Convert", "Source")
-valid_endings = ["mkv"]
+'''
+~ is the user home 
+- expanduser(~) will determine the correct user home regardless of the operating system
 
-format = ["av_mp4", "av_mkv", "av_webm"]
-codec = ["x265", "x265_10bit", "x265_12bit"]
+so the path will be 
+~/Convert/Result 
+and 
+~/Convert/Source
+respectively
+'''
+target_path = expanduser(path.join("~", "Convert", "Result"))
+source_path = expanduser(path.join("~", "Convert", "Source"))
 
-file_options = "--input {input} --output {output} --format {format} --markers "
-video_options = "--encoder {codec} --quality 20 --two-pass "
-audio_options = "--audio 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 --aencoder copy --audio-fallback he-aac "
+'''
+File endings which will be queued for conversion
+'''
+valid_endings = ["mkv", "m4v", "mp4"]
+
+'''
+Index for the target container file. 0 based
+'''
+selected_container = 1
+container = ["av_mp4", "av_mkv", "av_webm"]
+container_filetype = ["mp4", "mkv", "webm"]
+
+'''
+he-aac is the higher quality audio codec but only available on macos.
+'''
+fallback_audio = ["he-aac", "aac"]
+
+'''
+x265 encoders.
+10 bit has a quality improvement over the 8 bit standard x265 regarding rounding errors
+So the default will be 10bit
+'''
+selected_encoder = 1
+encoder = ["x265", "x265_10bit", "x265_12bit"]
+
+'''
+General input parameters like format, source and output
+'''
+input_arg = ["--input"]
+output_arg = ["--output"]
+format_arg = ["--format"]
+markers_arg = ["--markers"]
+
+'''
+Video options for quality 20 (lower number for higher quality)
+'''
+video_quality = "20"
+video_options = "--encoder {codec} --quality {quality} --two-pass "
+
+'''
+audio options. audio tracks which aren't present will be skipped, so just enumerate from 1 to 15
+copy all audio tracks and if not possible use fallback audio
+'''
+audio_options = "--audio 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 --aencoder copy --audio-fallback {audio_fallback} "
+
+'''
+Same as audio options. Tracks which aren't present will be skipped, so just enumerate from 1 to 15
+'''
 subtitles_options = "--subtitle 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 "
 
 
+def get_audio_fallback():
+    """
+    If this script is running on mac os, use the higher quality fallback 'he-aac' codec.
+    Otherwise default to "normal" 'aac'
+    """
+    system = platform.system()
+    if system == 'Darwin':
+        return fallback_audio[0]
+    return fallback_audio[1]
+
+
 def main():
-    files = find_files()
+    files_to_convert, files_to_copy = find_files()
+    os.makedirs(target_path, exist_ok=True)
 
-    for file in files:
-        real_path = expanduser(resultpath)
-        os.makedirs(real_path, exist_ok=True)
-        already_done = False
-        with open(path.join(real_path, journal)) as f:
-            lines = f.readlines()
-            if path.basename(file) in lines:
-                already_done = True
+    """
+    Copy non-convertible files to the result directory to keep the folder and file structure.
+    Will copy things like thumbnails and subtitles
+    """
+    copy_non_convert_files(files_to_copy, target_path)
 
-        if not already_done:
-            result_file = path.join(real_path, path.basename(file))
-            file_cmd = file_options.format(input=file, output=result_file, format=format[1])
-            video_cmd = video_options.format(codec=codec[1])
-            audio_cmd = audio_options
-            subtitle_cmd = subtitles_options
+    for file in files_to_convert:
+        convert_file = check_journal(file, target_path)
 
-            cmd = ("handbrakeCLI " + file_cmd + video_cmd + audio_cmd + subtitle_cmd).split()
+        if convert_file:
+            target_dir = path.dirname(file).replace(source_path, target_path)
+            target = path.join(target_dir, path.basename(file))
+            os.makedirs(target_dir, exist_ok=True)
+            if path.exists(target):
+                os.remove(target)
+
+            cmd = create_command(file, target)
             subprocess.run(cmd)
-            journal_file = open(path.join(real_path, journal), 'a')
-            journal_file.write(path.basename(file))
+            write_journal(file, target_path)
+
+
+def write_journal(file, real_path):
+    journal_file = open(path.join(real_path, journal), 'a')
+    journal_file.write(path.basename(file))
+
+
+def create_command(file, target):
+    input_arg.append(file)
+    target = create_target_filename(target)
+    output_arg.append(target)
+    format_arg.append(container[selected_container])
+    file_cmd = input_arg + output_arg + format_arg + markers_arg
+    video_cmd = video_options.format(codec=encoder[selected_encoder]).split()
+    audio_cmd = audio_options.format(audio_fallback=get_audio_fallback()).split()
+    subtitle_cmd = subtitles_options.split()
+    cmd = ["handbrakeCLI"] + file_cmd + video_cmd + audio_cmd + subtitle_cmd
+    print(cmd)
+    return cmd
+
+
+def create_target_filename(target):
+    split = str(target).split(".")
+    ending = split[len(split) - 1]
+    target = target.replace("." + ending, "." + container_filetype[selected_container])
+    target = target.replace("h264", "h265")
+    return target
+
+
+def check_journal(file, real_path):
+    """
+    If the file is already registered in the journal the script will not
+    attempt to convert it again
+    """
+    with open(path.join(real_path, journal)) as f:
+        lines = f.readlines()
+        if path.basename(file) in lines:
+            print(path.basename(file) + " was already processed. Skipping ...")
+            return False
+    return True
+
+
+def copy_non_convert_files(files_to_copy, target_path):
+    for file in files_to_copy:
+        target_dir = path.dirname(file).replace(source_path, target_path)
+        os.makedirs(target_dir, exist_ok=True)
+        target = path.join(target_dir, path.basename(file))
+        if not path.exists(target):
+            shutil.copyfile(file, target)
+            print("Copied " + file + " to " + target)
 
 
 def find_files():
-    absolute_paths = []
-    real_path = expanduser(sourcepath)
-    for (dirpath, dirname, filenames) in walk(real_path):
+    files_to_convert = []
+    files_to_copy = []
+    for (dirpath, _, filenames) in walk(source_path):
 
         for filename in filenames:
             split = str(filename).split(".")
             if split[len(split) - 1] in valid_endings:
-                absolute_paths.append(path.join(dirpath, filename))
+                files_to_convert.append(path.join(dirpath, filename))
+            elif not str(filename).startswith("."):
+                files_to_copy.append(path.join(dirpath, filename))
 
-    return absolute_paths
+    return files_to_convert, files_to_copy
 
 
 if __name__ == '__main__':
